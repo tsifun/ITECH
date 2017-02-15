fd14a0115c4b5f070d1d1149b25bf4605679b70d
Aslan Mirzakanov <2234581m@BO1028-3-08.ad.dcs.gla.ac.uk>
2017-02-15 13:52:15 +0000

    chapter 7

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
-
+from rango.models import Category
+from rango.models import Page
 from django.http import HttpResponse
+from rango.forms import CategoryForm
 
+def add_category(request):
+     form = CategoryForm()
+     if request.method == 'POST':
+	         form = CategoryForm(request.POST)
+	         if form.is_valid():
+                  form.save(commit=True)
+	          return index(request)
+	         else:
+                  print(form.errors)
+     return render(request, 'rango/add_category.html', {'form': form})			 
 def index(request):
-     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	
-# Return a rendered response to send to the client.
-# We make use of the shortcut function to make our lives easier.
-# Note that the first parameter is the template we wish to use.
-     return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
+
+    return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
 
